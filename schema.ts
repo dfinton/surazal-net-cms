3f8e66135b8b00a7a4fe14d421135d3a311d5a6e
David Finton <dave.finton@gmail.com>
2022-11-08 23:02:24 -0600

    disabled heading levels 1 and 2

--- schema.ts
+++ schema.ts
@@ -74,7 +74,32 @@ export const lists: Lists = {
       // the document field can be used for making rich editable content
       //   you can find out more at https://keystonejs.com/docs/guides/document-fields
       content: document({
-        formatting: true,
+        formatting: {
+          inlineMarks: {
+            bold: true,
+            italic: true,
+            underline: true,
+            strikethrough: true,
+            code: true,
+            superscript: true,
+            subscript: true,
+            keyboard: true,
+          },
+          listTypes: {
+            ordered: true,
+            unordered: true,
+          },
+          alignment: {
+            center: true,
+            end: true,
+          },
+          headingLevels: [3, 4, 5, 6],
+          blockTypes: {
+            blockquote: true,
+            code: true
+          },
+          softBreaks: true,
+        },
         layouts: [
           [1, 1],
           [1, 1, 1],
