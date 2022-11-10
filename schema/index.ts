4eb451d98133ac80aa03c49c2a84297aca37a41c
David Finton <dave.finton@gmail.com>
2022-11-09 19:12:53 -0600

    split schema up into multiple files

---
+++ schema/index.ts
@@ -0,0 +1,11 @@
+import type { Lists } from '.keystone/types';
+
+import User from './user';
+import Post from './post';
+import Tag from './tag';
+
+export const lists: Lists = {
+  User,
+  Post,
+  Tag,
+};
